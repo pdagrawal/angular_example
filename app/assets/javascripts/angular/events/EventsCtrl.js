7e1abecc806f51c9a4238c2b132959a769028924
Pradeep Agrawal <pdevagrawal@gmail.com>
2017-05-07 17:35:04 +0530

    Add new events

--- app/assets/javascripts/angular/events/EventsCtrl.js
+++ app/assets/javascripts/angular/events/EventsCtrl.js
@@ -8,4 +8,28 @@ app.controller('EventsCtrl', ['$scope', function($scope) {
     { name: 'Event 4', event_date: '04/10/2016', description: 'Description #4', place: 'Place #4'},
     { name: 'Event 5', event_date: '05/10/2016', description: 'Description #5', place: 'Place #5'}
   ];
+
+  $scope.addEvent = function() {
+    if (!valid()) {
+      return false;
+    }
+
+    $scope.events.push({
+      name: $scope.event.name,
+      event_date: $scope.event.event_date,
+      description: $scope.event.description,
+      place: $scope.event.place
+    });
+
+    $scope.event.name = '';
+    $scope.event.description = '';
+    $scope.event.event_date = '';
+    $scope.event.place = '';
+  };
+
+  valid = function() {
+    return !!$scope.event &&
+      !!$scope.event.name && !!$scope.event.event_date &&
+      !!$scope.event.description && !!$scope.event.place;
+  }
 }]);
